31516423dbb042204a118f570df9949b3bc9a950
cvtran <cvtran31@gmail.com>
2020-02-27 14:19:32 -0800

    Update Locators2.java
    Regular expressions for non-complete values

--- Locators/src/Locators2.java
+++ Locators/src/Locators2.java
@@ -13,9 +13,20 @@ public class Locators2 {
 		TimeUnit.SECONDS.sleep(1);
 		
 		driver.findElement(By.id("username")).sendKeys("hello"); //type "hello" into email text input
+		
+		//search for an input tag with a type that contains email and types "hello"
+		//regular expression - //tagName[contains(@attribute, 'value')]
+		//driver.findElement(By.xpath("//input[contains(@type, 'email')]")).sendKeys("hello");
+		
+		//search for an input tag with an id that contains username and types "hello"
+		//regular expression - tagName[attribute *= 'value']
+		//driver.findElement(By.cssSelector("input[id*='username']")).sendKeys("hello");
 		TimeUnit.SECONDS.sleep(1);
 		
+		
+		
 		driver.findElement(By.cssSelector("#password")).sendKeys("123456"); //type "123456" into password text input
+		//driver.findElement(By.cssSelector("input#password")).sendKeys("123456"); //same as above but makes sure the element has the input tag
 		TimeUnit.SECONDS.sleep(1);
 		
 		//driver.findElement(By.xpath("//*[@id=\'forgot_password_link\']")).click(); //click forgot password link
