b07ac751805bc99edc2c4908bd7fab4d1ef33137
cvtran <cvtran31@gmail.com>
2020-02-26 16:55:16 -0800

    Locators 2
    Xpath and CSS locators

--- Locators/src/Locators2.java
+++ Locators/src/Locators2.java
@@ -3,25 +3,25 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Locators {
-	
+public class Locators2 {
 	public static void main(String[] args) throws InterruptedException {
 		
-		//start webdriver
 		System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chomedriver_win32\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		
-		driver.get("http://facebook.com");
-		
-		driver.findElement(By.id("email")).sendKeys("hello"); //type "hello" into email text input
+		driver.get("https://login.salesforce.com");
 		TimeUnit.SECONDS.sleep(2);
 		
-		driver.findElement(By.name("pass")).sendKeys("123456"); //type "123456" into password text input
+		driver.findElement(By.id("username")).sendKeys("hello"); //type "hello" into email text input
 		TimeUnit.SECONDS.sleep(2);
 		
-		driver.findElement(By.id("loginbutton")).click(); //click login button
+		driver.findElement(By.cssSelector("#password")).sendKeys("123456"); //type "123456" into password text input
+		TimeUnit.SECONDS.sleep(2);
+		
+		driver.findElement(By.xpath("//*[@id=\'forgot_password_link\']")).click(); //click forgot password link
 		TimeUnit.SECONDS.sleep(5);
 		
 		driver.quit();
 	}
+	
 }
