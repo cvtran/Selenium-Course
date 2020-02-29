ef589c41cb9854262e8108501ce4133a09cfea67
cvtran <cvtran31@gmail.com>
2020-02-28 16:03:00 -0800

    Update Dropdown.java
    Changed name of file

--- Web-Elements/src/Dropdown.java
+++ Web-Elements/src/Dropdown.java
@@ -6,13 +6,13 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class SouthwestWebElements {
+public class Dropdown {
 	
 	public static void main(String[] args) throws InterruptedException {
 		
+		//start webdriver and navigate to southwest.com
 		System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chomedriver_win32\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
-		
 		driver.get("http://southwest.com");
 		TimeUnit.SECONDS.sleep(1);
 		
